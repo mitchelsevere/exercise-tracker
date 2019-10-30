d28e7fe1a69b1ef0c402b90ef8bf6dc052a6d104
mitchelsevere <mitchelsevere@gmail.com>
2019-10-29 21:10:31 -0400

    add mongoose connection

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
-
+// express server
 const app = express();
 const port = process.env.PORT || 5000;
-
+// middleware
 app.use(cors());
 app.use(express.json());
+// mongodb and mongoose connections
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB database connection establised successfully');
+});
 
+// starts server
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 })
