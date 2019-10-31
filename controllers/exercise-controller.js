5553045a3c339ad9491d4a862ddc8d580aaa0867
mitchelsevere <mitchelsevere@gmail.com>
2019-10-30 20:33:13 -0400

    finish backend

--- controllers/exercise-controller.js
+++ controllers/exercise-controller.js
@@ -2,18 +2,24 @@ const Exercise = require('../models/exercise');
 
 const exerciseController = {};
 
-exerciseController.show = (req, res) => {
-    Exercise.find()
+exerciseController.show = async (_, res) => {
+    await Exercise.find()
     .then(exercises => res.json(exercises))
     .catch(err => res.status(400).json(`Error: ${err}`)); 
 }
 
-exerciseController.add = (req, res) => {
+exerciseController.showOne = async (req, res) => {
+    await Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json(`Error ${err}`));
+}
+
+exerciseController.add = async (req, res) => {
     const username = req.body.username;
     const description = req.body.description;
     const duration = Number(req.body.duration);
     const date = Date.parse(req.body.date);
-
+    
     const newExercise = new Exercise({
         username,
         description,
@@ -21,8 +27,29 @@ exerciseController.add = (req, res) => {
         date
     });
 
-    newExercise.save()
+    await newExercise.save()
     .then(() => res.json('Exercise Added!'))
+    .catch(err => res.status(400).json(`Error: ${err}`));
+}
+
+exerciseController.update = async (req, res) => {
+    await Exercise.findById(req.params.id)
+    .then(exercise => {
+        exercise.username = req.body.username,
+        exercise.description = req.body.description,
+        exercise.duration = Number(req.body.duration),
+        exercise.date = Date.parse(req.body.date)
+
+        exercise.save()
+        .then(() => res.json('Exercise Updated'))
+        .catch(err => res.status(400).json(`Error ${err}`));
+    })
+    .catch(err => res.status(400).json(`Error: ${err}`));
+}
+
+exerciseController.remove = async (req, res) => {
+    await Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Exercise deleted.'))
     .catch(err => res.status(400).json(`Error: ${err}`));
 }
 
